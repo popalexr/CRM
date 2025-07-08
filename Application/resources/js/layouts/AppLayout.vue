2a13efab50e23453229cfffda8424bca1c043527
popalexr <alexpop8@hotmail.com>
2025-07-08 15:17:48 +0300

    Added Toast for error messages.

--- Application/resources/js/layouts/AppLayout.vue
+++ Application/resources/js/layouts/AppLayout.vue
@@ -1,6 +1,11 @@
 <script setup lang="ts">
 import AppLayout from '@/layouts/app/AppSidebarLayout.vue';
 import type { BreadcrumbItemType } from '@/types';
+import { usePage } from '@inertiajs/vue3';
+import { onMounted } from 'vue';
+
+import 'vue-sonner/style.css'
+import { toast, Toaster } from 'vue-sonner';
 
 interface Props {
     breadcrumbs?: BreadcrumbItemType[];
@@ -9,10 +14,24 @@ interface Props {
 withDefaults(defineProps<Props>(), {
     breadcrumbs: () => [],
 });
+
+const page = usePage();
+
+const error = page.props.flash.error || null;
+
+onMounted(() => {
+    if (error) {
+        toast.error(error, {
+            duration: 5000
+        });
+    }
+})
+
 </script>
 
 <template>
     <AppLayout :breadcrumbs="breadcrumbs">
         <slot />
+        <Toaster  />
     </AppLayout>
 </template>
