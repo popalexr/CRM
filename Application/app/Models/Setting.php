1f219db48839bda5ba762add7632c8130af0197e
Marian-Gabriel Pop-Cristof <149391457+marianpop03@users.noreply.github.com>
2025-07-10 11:04:01 +0300

    Add Setting model and migration for settings table

---
+++ Application/app/Models/Setting.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Setting extends Model
+{
+    use HasFactory;
+    protected $table = 'settings';
+    protected $primaryKey = 'key';
+    public $incrementing = false;
+    protected $keyType = 'string';
+
+    protected $fillable = [
+        'key',
+        'type',
+        'value',
+        
+    ];
+}
