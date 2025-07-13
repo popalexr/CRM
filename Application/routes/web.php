3e90cfe4114cdc3c62f05f559e30bc58b1cce7e2
popalexr <alexpop8@hotmail.com>
2025-07-13 23:29:08 +0300

    Add users route file.

--- Application/routes/web.php
+++ Application/routes/web.php
@@ -2,7 +2,6 @@
 
 use App\Http\Controllers\Dashboard\DashboardController;
 use Illuminate\Support\Facades\Route;
-use Inertia\Inertia;
 
 Route::get('/', DashboardController::class)->middleware(['auth', 'verified'])->name('home');
 
@@ -10,5 +9,6 @@ Route::get('/', DashboardController::class)->middleware(['auth', 'verified'])->n
 require __DIR__.'/profile.php';
 require __DIR__.'/auth.php';
 require __DIR__.'/clients.php';
+require __DIR__.'/users.php';
 require __DIR__.'/settings.php';
 require __DIR__.'/upload.php';
