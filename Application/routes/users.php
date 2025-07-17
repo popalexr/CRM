fe43e554dd02946ada83e9db1f74441838c14006
Marian-Gabriel Pop-Cristof <149391457+marianpop03@users.noreply.github.com>
2025-07-17 23:37:18 +0300

    Fix duplicate route definitions for user form and deletion

--- Application/routes/users.php
+++ Application/routes/users.php
@@ -8,13 +8,13 @@ use App\Http\Controllers\Users\UserDetailsController;
 Route::middleware('auth')->prefix('users')->group(function () {
     Route::get('/', UsersController::class)->name('users.index');
     Route::get('/form', UsersFormController::class)->name('users.form');
-<<<<<<< HEAD
+
     Route::post('/form', UsersFormController::class)->name('users.form.post');
      Route::get('/{user}', UserDetailsController::class)->name('users.show'); 
     Route::delete('/', DeleteUserController::class)->name('users.destroy');
-=======
+
     
     Route::post('/form', [UsersFormController::class, 'post'])->name('users.form.post');
     Route::post('/', DeleteUserController::class)->name('users.destroy');
->>>>>>> a4db04d8ef8b1cf26b53fac209557085e8dfed5e
+
 });
