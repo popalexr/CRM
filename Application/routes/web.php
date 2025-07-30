489d2e231e4d958393021bafe23cfe4aba0748bd
popalexr <alexpop8@hotmail.com>
2025-07-30 20:09:06 +0300

    Remove unused files.

--- Application/routes/web.php
+++ Application/routes/web.php
@@ -2,14 +2,9 @@
 
 use App\Http\Controllers\Dashboard\DashboardController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\TestInvoiceDesignController;
 
 Route::get('/', DashboardController::class)->middleware(['auth', 'verified'])->name('home');
 Route::post('/dashboard/reminder', [DashboardController::class, 'saveReminder'])->middleware(['auth'])->name('dashboard.reminder');
-
-Route::get('/invoice/design1', [TestInvoiceDesignController::class, 'design1']);
-Route::get('/invoice/design2', [TestInvoiceDesignController::class, 'design2']);
-Route::get('/invoice/design3', [TestInvoiceDesignController::class, 'design3']);
 
 require __DIR__.'/profile.php';
 require __DIR__.'/auth.php';
